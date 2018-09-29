8bf9f26da5e35cc5574f8e00ac8f8122977fa008
Josh Remaley <joshremaley@gmail.com>
2018-09-29 14:38:40 -0500

    readme

--- server/config.py
+++ server/config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config(object):
 	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = 'postgres://josh@localhost:5432/test'
+	SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SECRET_KEY = "HALOinvestingIStheBEST"
